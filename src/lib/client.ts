5074d94ada2da590744001e8050b851921e683d4
mkh-web <mohnd.khalid2001@gmail.com>
2024-07-14 07:50:47 +0300

    start working on the final

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client'
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma = globalForPrisma.prisma || new PrismaClient()
-let a
+
 export default prisma // مؤقت لين البرودكشن تايم
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma//هذا كله عشان ما يسوي اكثر من كونكشن سترنق في اتصال واحد
